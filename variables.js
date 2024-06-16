9e823461b8f258895326107b22a314d3399d546f
RRawal369 <rrawal0100@gmail.com>
2024-06-16 12:59:34 +0530

    adding script for variable sin JS

---
+++ variables.js
@@ -0,0 +1,61 @@
+// var a = 10;
+// console.log(a);
+
+// // this definition
+
+// a = "hello";
+// console.log(a);
+
+// var a = 10;
+// console.log(a);
+
+// // this declaration
+
+// var a = "hello";
+// console.log(a);/
+
+
+//------------------ let keyword introduced by ES6 (2015) stops redeclaration --------------
+
+
+// let  a = 10;
+// console.log(a);
+
+// // this declaration
+
+// let a = "hello";  // not allowed
+// var a = "hello"; // not allowed
+// console.log(a);
+
+
+//------------------ let keyword allows redeclaration --------------
+
+
+// let  a = 10;
+// console.log(a);
+
+// // this definition
+
+//  a = "hello";  //  allowed
+// console.log(a);
+
+//------------------ const keyword blocks redeclaration and redefinition both--------------
+
+
+const  a = 10;
+console.log(a);
+
+a = "hello";  
+console.log(a);
+
+
+//                  redeclaration             redefinition
+
+// var                 Y                           Y
+// let                 N                           Y
+// const               N                           N 
+
+
+
+
+
